351eee8ec3576e127d41a86b0e4d2217e717c22f
emjayMJkim <minjeong030420@gmail.com>
2025-09-11 03:08:15 +0900

    #1 [add] 프로젝트 생성

---
+++ composeApp/src/commonTest/kotlin/com/tdd/bookshelf/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.tdd.bookshelf
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/tdd/bookshelf/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.tdd.bookshelf
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
