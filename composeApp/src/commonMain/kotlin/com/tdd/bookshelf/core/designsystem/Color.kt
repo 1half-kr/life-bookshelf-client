4e22388214b1420ee6302252364eb3b2bb6321bc
emjayMJkim <minjeong030420@gmail.com>
2025-09-14 04:57:39 +0900

    #1 [add] design-system color 세팅

---
+++ composeApp/src/commonMain/kotlin/com/tdd/bookshelf/core/designsystem/Color.kt
@@ -0,0 +1,40 @@
+package com.tdd.bookshelf.core.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+// BackGround
+val BackGround1 = Color(0xFFECEBF0)
+val BackGround2 = Color(0xFFF7F6FB)
+
+// White
+val White0 = Color(0xFFFFFFFF)
+
+// Black
+val Black0 = Color(0xFF000000)
+val Black100 = Color(0xFF181D27)
+
+// Blue
+val Blue500 = Color(0xFF3B4054)
+val Blue600 = Color(0xFF567AF3)
+val Blue800 = Color(0xFF0B4870)
+val Blue850 = Color(0xFF0601B4)
+val Blue900 = Color(0xFF2B4BF2)
+
+// Gray
+val Gray50 = Color(0xFFF5F9FE)
+val Gray100 = Color(0xFFE8E8E8)
+val Gray200 = Color(0xFFA2A2A2)
+val Gray300 = Color(0xFFABABAB)
+val Gray400 = Color(0xFFADADAD)
+val Gray500 = Color(0xFF838999)
+val Gray600 = Color(0xFF7C8BA0)
+val Gray800 = Color(0xFF3B4054)
+val Gray900 = Color(0xFF262626)
+
+// Neutral
+val Neutral200 = Color(0xFF848FAC)
+val Neutral500 = Color(0xFF848FAC)
+val Neutral900 = Color(0xFF192252)
+
+// Red
+val Red100 = Color(0xFDB2A12)
