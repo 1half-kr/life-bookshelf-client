0031f9c97a428080e5bd85c5ee574ccf15b82c30
emjayMJkim <minjeong030420@gmail.com>
2025-09-18 00:12:35 +0900

    #3 [mod] status bar padding 추가

--- composeApp/src/commonMain/kotlin/com/tdd/bookshelf/feature/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/tdd/bookshelf/feature/MainScreen.kt
@@ -1,13 +1,11 @@
 package com.tdd.bookshelf.feature
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.tdd.bookshelf.core.navigation.NavRoutes
@@ -20,7 +18,6 @@ fun MainScreen() {
     val navController = rememberNavController()
 
     Scaffold(
-        contentWindowInsets = WindowInsets(0.dp),
         bottomBar = {},
         snackbarHost = {}
     ) { innerPadding ->
